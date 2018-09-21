a0c78943377dc81a146a8d997e0df229a617c1d9
julKali <julian.kalitzke@gmx.at>
2018-09-21 21:13:37 +0200

    Added startup class Program

---
+++ MessengerTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using JulKali.Facebook.Messenger;
+
+namespace MessengerTests
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new MessengerClient(Environment.GetEnvironmentVariable("FB_ACCESS_TOKEN"));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
