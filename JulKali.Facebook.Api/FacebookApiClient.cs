3a74b5932e71bbdb444dce8880f476bfd9fbb8ac
julKali <julian.kalitzke@gmx.at>
2018-09-21 21:05:39 +0200

    Added FacebookApiClient

---
+++ JulKali.Facebook.Api/FacebookApiClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+
+namespace JulKali.Facebook.Api
+{
+    public class FacebookApiClient
+    {
+        private readonly HttpClient _client;
+
+        public FacebookApiClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public FacebookApiClient(HttpClient client)
+        {
+            _client = client;
+        }
+    }
+}
