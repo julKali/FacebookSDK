eacee3eb31104a3be77dbf0eb355af890ae86a65
julKali <julian.kalitzke@gmx.at>
2018-09-21 21:46:35 +0200

    Added option to supply name

--- JulKali.Facebook.Messenger/PhoneNumberRecipient.cs
+++ JulKali.Facebook.Messenger/PhoneNumberRecipient.cs
@@ -2,13 +2,38 @@
 {
     public class PhoneNumberRecipient : Recipient
     {
+        private readonly string _firstName;
+        private readonly string _lastName;
+        private readonly bool _nameSupplied;
+
         public PhoneNumberRecipient(string phoneNumber) 
             : base(phoneNumber)
         {
         }
 
+        public PhoneNumberRecipient(string phoneNumber, string firstName, string lastName)
+            : base(phoneNumber)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _nameSupplied = true;
+        }
+
         public override object ToRecipientJsonObject()
         {
+            if (_nameSupplied)
+            {
+                return new
+                {
+                    phone_number = Identifier,
+                    name = new
+                    {
+                        first_name = _firstName,
+                        last_name = _lastName
+                    }
+                };
+            }
+
             return new
             {
                 phone_number = Identifier
