8f188b9ce73f340a21e041c5c67a5d5af6e78d28
julKali <julian.kalitzke@gmx.at>
2018-09-21 21:10:23 +0200

    Added MessengerClient

---
+++ JulKali.Facebook.Messenger/MessengerClient.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using JulKali.Facebook.Api;
+
+namespace JulKali.Facebook.Messenger
+{
+    public class MessengerClient
+    {
+        private readonly string _accessToken;
+        private readonly FacebookApiClient _client;
+
+        private const string ApiEndpoint = "https://graph.facebook.com/me/messages";
+
+        public MessengerClient(string accessToken)
+        {
+            _accessToken = accessToken;
+            _client = new FacebookApiClient();
+        }
+
+        public MessengerClient(string accessToken, FacebookApiClient client)
+        {
+            _accessToken = accessToken;
+            _client = client;
+        }
+
+        public async Task<bool> SendText(Recipient recipient, string text, MessageType messageType = MessageType.Standard, MessagingType messagingType = MessagingType.Response)
+        {
+            string messagingTypeString;
+
+            switch (messagingType)
+            {
+                case MessagingType.Response:
+                    messagingTypeString = "RESPONSE";
+                    break;
+
+                case MessagingType.MessageTag:
+                    messagingTypeString = "MESSAGE_TAG";
+                    break;
+
+                case MessagingType.Update:
+                    messagingTypeString = "UPDATE";
+                    break;
+
+                default:
+                    throw new MessagingTypeNotSupportedException(messagingType);
+            }
+
+            var message = new
+            {
+                messaging_type = messagingTypeString,
+                recipient = recipient.ToRecipientJsonObject(),
+                message = new
+                {
+                    text
+                }
+            };
+
+            return await PostToEndpoint(message);
+        }
+    }
+}
