cb7d9748c05a5f7f041ed8436bcdff6a3588a580
julKali <julian.kalitzke@gmx.at>
2018-09-21 21:10:14 +0200

    Added MessagingTypeNotSupportedException

---
+++ JulKali.Facebook.Messenger/MessagingTypeNotSupportedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JulKali.Facebook.Messenger
+{
+    public class MessagingTypeNotSupportedException : Exception
+    {
+        public MessagingTypeNotSupportedException(MessagingType type) 
+            : base($"Invalid messaging type: {type.ToString()}")
+        {
+        }
+    }
+}
