f041b2056037d9ad4bf57fa03fbcddff53f6e0f1
julKali <julian.kalitzke@gmx.at>
2018-09-22 15:09:36 +0200

    Added example code

--- MessengerTests/Program.cs
+++ MessengerTests/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using JulKali.Facebook.Messenger;
+using JulKali.Facebook.Messenger.Send;
 
 namespace MessengerTests
 {
@@ -8,9 +9,21 @@ namespace MessengerTests
     {
         static async Task Main(string[] args)
         {
+            // initialize client
+
             var client = new MessengerClient(Environment.GetEnvironmentVariable("FB_ACCESS_TOKEN"));
 
-            Console.WriteLine("Hello World!");
+            // initialize recipient
+
+            var rec = new PsidRecipient(Environment.GetEnvironmentVariable("TEST_PSID"));
+
+            // send SenderAction "typing on"
+
+            await client.SendAction(rec, SenderAction.TypingOn);
+
+            // send example text
+
+            await client.SendText(rec, "This is an example message");
         }
     }
 }
